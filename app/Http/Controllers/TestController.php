8c962f5e040c08cea620b8c55a47e817a4852a64
amabdelftah <ahmedabdelftah95165@gmail.com>
2021-03-13 13:11:10 +0200

    Move logic to TestController

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,8 +8,13 @@ class TestController extends Controller
 {
     public function testAction()
     {
-        $name = 'asdasd';
+        $posts = [
+            ['id' => 1, 'title' => 'Laravel', 'posted_by' => 'Ahmed', 'created_at' => '2021-03-13'],
+            ['id' => 2, 'title' => 'JS', 'posted_by' => 'Mohamed', 'created_at' => '2021-03-25'],
+        ];
 
-        return $name;
+        return view('test', [
+            'posts' => $posts,
+        ]);
     }
 }
