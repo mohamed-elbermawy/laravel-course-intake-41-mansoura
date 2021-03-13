7e80da81221f484ad2ff914c787a06a7f9c464f8
amabdelftah <ahmedabdelftah95165@gmail.com>
2021-03-13 15:01:18 +0200

    Add layouts/app

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-</head>
-<body>
- <!-- As a link -->
-<nav class="navbar navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">All Posts</a>
-    </div>
-  </nav>
+@extends('layouts.app')
+
+@section('content')
 <a href="#" class="btn btn-success">Create Post</a>
 
     <table class="table  mt-5 container">
@@ -42,7 +29,5 @@
           @endforeach
         </tbody>
       </table>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
+    
